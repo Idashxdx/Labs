b41846bade2cd190945785462a2363e5580b2539
Aida <mashrabova2003@bk.ru>
2023-12-02 17:01:34 +0300

    correction 1.10

--- MP/lab1/lab1.10/action.c
+++ MP/lab1/lab1.10/action.c
@@ -10,21 +10,27 @@ check_input_data check_base(int base)
 }
 check_input_data check_number(char *number, int base)
 {
-    if (*number < 0)
+    if ((*number == '-' && strlen(number) > 1) || (*number >= '0' && *number <= '9') || (*number >= 'A' && *number <= ('A' + base - 11)))
     {
-        *number = -*number;
+        return correct_data;
     }
-    if (*number < '0' || (*number > '9' && *number < 'A') || *number > ('A' + base - 11))
-    {
-        return incorrect_data;
-    }
-    return correct_data;
+    return incorrect_data;
 }
 int conversion_to_decimal(char *number, int base)
 {
+
+    int negative = 1;
+    int start_position = 0;
+
+    if (number[0] == '-')
+    {
+        negative = -1;
+        start_position = 1;
+    }
+
     int decimal = 0;
     int digit;
-    for (int i = 0; number[i] != '\0'; i++)
+    for (int i = start_position; number[i] != '\0'; i++)
     {
         if (isdigit(number[i]))
         {
@@ -36,7 +42,7 @@ int conversion_to_decimal(char *number, int base)
         }
         decimal = decimal * base + digit;
     }
-    return decimal;
+    return decimal * negative;
 }
 
 int find_max_number(int number_1, int number_2)
@@ -82,22 +88,34 @@ void representation_at_base(int max_number, int base)
 {
     char result[100];
     int position = 0;
+    int is_negative = 0;
+
+    if (max_number < 0)
+    {
+        max_number = -max_number;
+        is_negative = 1;
+    }
 
     while (max_number > 0)
     {
         int remainder = max_number % base;
         if (remainder < 10)
         {
-            result[position++] = remainder + '0';
+            result[position] = remainder + '0';
         }
         else
         {
-            result[position++] = remainder - 10 + 'A';
+            result[position] = (remainder - 10) + 'A';
         }
+        position++;
         max_number /= base;
     }
-
     printf("\n Представление с основанием %d: ", base);
+    if (is_negative)
+    {
+        printf("-");
+    }
+
     for (int i = position - 1; i >= 0; i--)
     {
         printf("%c", result[i]);
