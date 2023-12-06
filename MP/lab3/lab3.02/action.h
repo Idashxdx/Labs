6e4b9ddb82100649ea94dc97edf3f7d56d729f62
Aida <mashrabova2003@bk.ru>
2023-12-06 23:24:30 +0300

    create 3.02

---
+++ MP/lab3/lab3.02/action.h
@@ -0,0 +1,24 @@
+
+#ifndef action_h
+#define action_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+
+
+typedef struct {
+
+} Vector;
+
+typedef enum check_data
+{
+    correct_data,
+    incorrect_data,
+    memory_malloc_error,
+} check_data;
+
+#endif
