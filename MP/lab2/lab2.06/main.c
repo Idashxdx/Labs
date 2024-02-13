3d3bbc8f6b5f3170c042e13bc31a1452a9625542
Aida <mashrabova2003@bk.ru>
2024-02-14 01:23:24 +0300

    fix

--- MP/lab2/lab2.06/main.c
+++ MP/lab2/lab2.06/main.c
@@ -18,7 +18,6 @@ int main()
     printf("Decimal Number 1 ---> %d\n", cv_num_lower);
     printf("Decimal Number 2 ---> %u\n", cv_num_upper);
     fclose(file);
-
     char input_str[] = "XXI 11000 -af AF";
     int romanNumber2, zeckendorfNumber2, cv_num_lower2;
     unsigned int cv_num_upper2;
